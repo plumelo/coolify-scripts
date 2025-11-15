d535ff484034fb81086f7c4b3d6905f08e551687
Meghea Iulian <iulian@plumelo.com>
2025-11-15 18:29:36 +0200

    chore: adjust read dir

--- backup-volume.sh
+++ backup-volume.sh
@@ -3,7 +3,7 @@
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -v|--volume) volume="$2"; shift ;;
-        -d|--dir) dir=1 ;;
+        -d|--dir) dir="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
