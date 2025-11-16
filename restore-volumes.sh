1ebfe032bd339327f219742b471f2aa8767cc6da
Meghea Iulian <iulian@plumelo.com>
2025-11-16 08:32:15 +0200

    feat: restore volume cwd

--- restore-volumes.sh
+++ restore-volumes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+cdir="$(dirname "${BASH_SOURCE[0]}")"
 for i in $(docker volume ls -f "name=$1" -q); do
 	rep=$(echo $i | sed -r "s/(\w+)_/$2_/g")
-	./coolify-scripts/restore-volume.sh -v "$i" -f "$3/$rep.tar.gz"
+	$cdir/restore-volume.sh -v "$i" -f "$3/$rep.tar.gz"
 done
