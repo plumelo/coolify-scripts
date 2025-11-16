f840d6218a874def71c10e9afd51e103e2c47dc8
Meghea Iulian <iulian@plumelo.com>
2025-11-16 08:19:44 +0200

    feat: cwd

--- backup-volumes.sh
+++ backup-volumes.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker volume ls -f "name=$1" -q | xargs -I {} ./backup-volume.sh -v {} -d $2
+cdir="$(dirname "${BASH_SOURCE[0]}")"
+docker volume ls -f "name=$1" -q | xargs -I {} $cdir/backup-volume.sh -v {} -d $2
